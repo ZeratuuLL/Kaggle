1bed89ee86ca1303c6356bb026ecb2c53377e0f8
Shitong-Wei <swei@ucdavis.edu>
2018-11-24 16:49:12 -0800

    Delete customNull.py

--- customNull.py
+++
@@ -1,9 +0,0 @@
-def customNull(colname):
-    row_num=train.shape[0]
-    a=[]
-    for i in range(row_num):
-        for j in range(len(train[i])):
-            if train[i][j][colname]!=[]:
-                a.append([i,j])
-    return a
-
