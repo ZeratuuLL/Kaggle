a85045ed4baf213eb15e40e4986cb5ef2f830e84
Lifeng Wei <lfwei@ucdavis.edu>
2018-11-24 16:51:22 -0800

    Update and rename customNull.py to DataProcessing.py

---
+++ Google Analytics Customer Revenue Prediction/DataProcessing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+train = pd.read_pickle('Train_Saved.pkl')
+
+def custom_length(x):
+    keys = ['customVariables', 'customDimensions', 'customMetrics']
+    result = ()
+    for i in [0,-1]:
+        temp = ()
+        for j in keys:
+            temp += (x[i][j])
+        result += temp
+    return result
