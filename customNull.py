0e7b2b5b0f5679dbf15d396cd737c740fb73a0a5
Shitong-Wei <swei@ucdavis.edu>
2018-11-24 16:22:04 -0800

    function for custom... variables

---
+++ customNull.py
@@ -0,0 +1,12 @@
+
+
+
+def customNull(colname):
+    row_num=train.shape[0]
+    a=[]
+    for i in range(row_num):
+        for j in range(len(train[i])):
+            if train[i][j][colname]!=[]:
+                a.append([i,j])
+    return a
+
