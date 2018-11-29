983844c99d477f6bf745019d524fd247663060b8
Shitong-Wei <swei@ucdavis.edu>
2018-11-29 00:43:36 -0800

    Update content_post.py

--- Google Analytics Customer Revenue Prediction/content_post.py
+++ Google Analytics Customer Revenue Prediction/content_post.py
@@ -1,2 +1,7 @@
 import pandas as pd
+
 def join_content(x1="temp_table.pkl",x2="content_posterior.pkl"):
+  temp_table = pd.read_pickle(x1)
+  content_post = pd.read_pickle(x2)
+  new_table = temp_table.set_index("fullVisitorId").join(content_post)
+  return new_table.reset_index()
