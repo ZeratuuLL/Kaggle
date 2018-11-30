44251c74755fbe10b5f0586c9a47d135ed9cbabc
Lifeng Wei <lfwei@ucdavis.edu>
2018-11-29 23:46:51 -0800

    Update Merge.py

--- Google Analytics Customer Revenue Prediction/Merge.py
+++ Google Analytics Customer Revenue Prediction/Merge.py
@@ -81,9 +81,9 @@ def training_merge(c1=0.00001, c2=0.01):
     Training.reset_index(inplace=True)
     Training.drop(['index'], axis=1, inplace=True)
     Training.set_index('month_index', inplace=True)
-    #month_parameters = pd.read_pickle('train_month_parameters.pkl')
-    #Training = Training.join(month_parameters)
-    #del month_parameters
+    month_parameters = pd.read_pickle('train_month_parameters.pkl')
+    Training = Training.join(month_parameters)
+    del month_parameters
     
     Training.reset_index(inplace=True)
     Training.set_index('fullVisitorId', inplace=True)
@@ -189,9 +189,9 @@ def validate_merge(c1=0.00001, c2=0.01):
     validation.reset_index(inplace=True)
     validation.drop(['index'], axis=1, inplace=True)
     validation.set_index('month_index', inplace=True)
-    #month_parameters = pd.read_pickle('train_month_parameters.pkl')
-    #validation = validation.join(month_parameters)
-    #del month_parameters
+    month_parameters = pd.read_pickle('train_month_parameters.pkl')
+    validation = validation.join(month_parameters)
+    del month_parameters
     
     validation.reset_index(inplace=True)
     validation.set_index('fullVisitorId', inplace=True)
@@ -199,4 +199,91 @@ def validate_merge(c1=0.00001, c2=0.01):
     
     return validation
     
+def full_merge(c1=0.00001, c2=0.01):
+    '''
+    This function merges different sources into a single training set.
+    c1 is the weight for prior where fullVisitorId is index
+    c2 is the weight for other priors
+    '''
+    Full = pd.read_pickle('Full_Base.pkl')
+    Full['revenue'] = Full['revenue'].apply(lambda x:np.log(x+1))
     
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    Full['month_category'] = Full['month'].apply(lambda x: months[x-1])
+    Full = pd.concat([Full, pd.get_dummies(Full['month_category'])], axis=1)
+    Full.drop(['month_category'], axis=1, inplace=True)
+    
+    #join browser information
+    ID_Browser = pd.read_pickle('full_ID_Browser.pkl')
+    Full.set_index('fullVisitorId', inplace=True)
+    Full = Full.join(ID_Browser)
+    del ID_Browser
+    
+    #join content information
+    alphas = pd.read_pickle('full_content_alphas.pkl')
+    content = pd.read_pickle('full_content_prior.pkl')
+    content += alphas*c1
+    ncol = content.shape[1]
+    temp_sum = content.iloc[:,:4].sum(axis=1)
+    for i in range(4):
+        ratio = content.iloc[:,i]/temp_sum
+        content.iloc[:,i] = ratio
+    temp_sum = content.iloc[:,4:14].sum(axis=1)
+    for i in range(4,14):
+        ratio = content.iloc[:,i]/temp_sum
+        content.iloc[:,i] = ratio
+    temp_sum = content.iloc[:,14:ncol].sum(axis=1)
+    for i in range(14,ncol):
+        ratio = content.iloc[:,i]/temp_sum
+        content.iloc[:,i] = ratio
+    Full = Full.join(content)
+    del content, temp_sum
+    
+    #join mobile information
+    mobile = pd.read_pickle('full_mobile_prior.pkl')
+    mobile_parameters = np.load('full_mobile_parameters.npy')
+    (alpha, beta) = mobile_parameters
+    mobile['mobile'] = (c1*alpha+mobile['s'])/(c1*alpha+c1*beta+mobile['n'])
+    mobile = mobile['mobile']
+    Full = Full.join(mobile)
+    del mobile
+    
+    #join pageView information
+    pageView = pd.read_pickle('full_pageView_prior.pkl')
+    (alpha, beta) = np.load('full_pageView_parameters.npy')
+    pageView['beta'] = c1*beta + pageView['pageview']
+    pageView['alpha'] = c1*alpha + pageView['counts']
+    pageView['pageview_log'] = pageView['beta']/(pageView['alpha']-1)
+    pageView['pageView'] = pageView['pageview_log'].apply(np.exp)
+    pageView = pageView.iloc[:,-2:]
+    Full = Full.join(pageView)
+    del pageView    
+              
+    #join visitlength information
+    visitlength = pd.read_pickle('full_VisitLength.pkl')
+    Full = Full.join(visitlength)
+    del visitlength
+    
+    #join country information
+    Full.reset_index(inplace=True)
+    Full.set_index('country',inplace=True)
+    (nu, mu0, alpha, beta) = np.load('full_country_parameters.npy')
+    country = pd.read_pickle('full_country_prior.pkl')
+    country['country_param1'] = (c2*nu*mu0+country['n']*country['mu'])/(c2*nu+country['n'])
+    country['country_param2'] = (c2*alpha+country['raten'])/(c2*alpha+c2*beta+country['raten'])
+    country = country.iloc[:,-2:]
+    Full = Full.join(country)
+    del country
+    
+    #join monthly time series prediction
+    Full.reset_index(inplace=True)
+    Full.drop(['index'], axis=1, inplace=True)
+    Full.set_index('month_index', inplace=True)
+    month_parameters = pd.read_pickle('full_month_parameters.pkl')
+    Training = Training.join(month_parameters)
+    del month_parameters
+    
+    Full.reset_index(inplace=True)
+    Full.set_index('fullVisitorId', inplace=True)
+    
+    return Full
